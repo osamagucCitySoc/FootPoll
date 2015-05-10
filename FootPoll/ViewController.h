c0704eff3ea2831d777859ab608d6e815585683b
osamagucCitySoc <osamarabie@Osamas-MBP.Home>
2015-05-10 12:18:36 +0400

    Initial Commit

---
+++ FootPoll/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FootPoll
+//
+//  Created by Osama Rabie on 5/10/15.
+//  Copyright (c) 2015 Osama Rabie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FootPoll/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FootPoll
+//
+//  Created by Osama Rabie on 5/10/15.
+//  Copyright (c) 2015 Osama Rabie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
